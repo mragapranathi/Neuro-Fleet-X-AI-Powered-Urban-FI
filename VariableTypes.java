79b911cea2fb84248a78ed372b62f237682a44ef
mragapranathi <mragapranathi@gmail.com>
2025-08-15 18:06:16 +0530

    Add files via upload

---
+++ VariableTypes.java
@@ -0,0 +1,25 @@
+
+public class VariableTypes 
+{
+
+	public static void main(String[] args) 
+	{
+		byte b=2;
+		short s=4;
+		int i=6;
+		long l=8;
+		float f=9.5f;
+		double d=100.25;
+		char c='A';
+		boolean b1=true;
+		System.out.println("The value of b (Byte) = "+b);
+		System.out.println("The value of s (Short) = "+s);
+		System.out.println("The value of i (int) = "+i);
+		System.out.println("The value of l (Long) = "+l);
+		System.out.println("The value of f (Float) = "+f);
+		System.out.println("The value of d (Double) = "+d);
+		System.out.println("The value of c (Char) = "+c);
+		System.out.println("The value of b1 (Boolean) = "+b1);
+	}
+
+}
